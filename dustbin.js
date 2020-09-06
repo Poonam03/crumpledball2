0dc5625e0d9853ff71c8e2e0f019b9b547e7a88f
Poonam03 <68641541+Poonam03@users.noreply.github.com>
2020-09-06 23:35:48 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -6,6 +6,7 @@ class Dustbin
 		this.y=y;
         this.width=140;
 		this.height=140;
+		//height is given 0 for bodies so that the paper goes in dustbin else it form a boundary around dustbin and does not let paper fall in dustbin
 		this.body=Bodies.rectangle(x,y,this.width,0,{isStatic:true});
         this.image=loadImage("dustbingreen.png")
         World.add(world,this.body);
